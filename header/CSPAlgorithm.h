cfa8132a420f1cf71d178c1283c8a7b401201863
rafrusth <rafifsetya41@gmail.com>
2026-06-07 21:10:09 +0700

    fix: ids malaha da backtrack

--- header/CSPAlgorithm.h
+++ header/CSPAlgorithm.h
@@ -16,6 +16,7 @@ bool isCompatibleBt(PC pc, int n);
 void backtracking(int n, int *iteration, LListPC *ll_pc, PC pc, ListCPU l_cpu, ListMotherboard l_mobo, ListRAM l_ram, ListPSU l_psu, ListGPU l_gpu, ListStorage l_storage);
 
 // Iterative Deepening Search
+bool isCompatibleIds(PC pc);
 void limitDepth(int n, int limit, int *iteration, PC pc, ListCPU l_cpu, ListMotherboard l_mobo, ListRAM l_ram, ListPSU l_psu, ListGPU l_gpu, ListStorage l_storage, LListPC *ll_pc);
 void iterativeDeepeningSearch(int max_n, int *iteration, LListPC *ll_pc, PC pc, ListCPU l_cpu, ListMotherboard l_mobo, ListRAM l_ram, ListPSU l_psu, ListGPU l_gpu, ListStorage l_storage);
 
